181f578d85d75984fba123cc1c6a448e629c6250
KSH0660 <42088428+KSH0660@users.noreply.github.com>
2026-09-07 15:05:50 +0900

    Fix duplicate survey bank exports

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,4 +1,4 @@
-export {
+import {
   DEFAULT_SURVEY_IDS,
   SURVEY_BANK_VERSION,
   introQuestion,
@@ -7,9 +7,15 @@ export {
   surveyTopics,
 } from "./survey-bank";
 
-import { introQuestion, surveyQuestionCount, surveyTopicById, surveyTopics } from "./survey-bank";
+export {
+  DEFAULT_SURVEY_IDS,
+  SURVEY_BANK_VERSION,
+  introQuestion,
+  surveyQuestionCount,
+  surveyTopicById,
+  surveyTopics,
+};
 
 export const allTopics = surveyTopics;
 export const topicById = surveyTopicById;
 export const totalQuestionCount = surveyQuestionCount + 1;
-export { introQuestion };
